cb1404b9910b733610bd31658d8a6cfb6fe4a01c
john-harding <jsh.miami@gmail.com>
2015-06-19 20:06:31 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>John Harding Jr's Portfolio</title>
+<!-- Custom CSS - Compiled from LESS -->
+<link rel="stylesheet" href="css/style.css" type="text/css">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+ <!-- HTML5 Shim and Respond.js -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<header>
+<a href="/"><img src="images/logo.png"></a>
+
+</header>
+
+</body>
+</html>
