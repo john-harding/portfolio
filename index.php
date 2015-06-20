45cd1d845ba317127bd6d4f9fe84df73ed4f4ea5
john-harding <jsh.miami@gmail.com>
2015-06-20 17:07:43 -0600

    Update header, add images, add styles

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>John Harding Jr's Portfolio</title>
 <!-- Custom CSS - Compiled from LESS -->
 <link rel="stylesheet" href="css/style.css" type="text/css">
@@ -16,9 +19,34 @@
 </head>
 <body>
 <header>
-<a href="/"><img src="images/logo.png"></a>
-
+<a href="/"><img src="images/logo-white.png" alt="John Harding Jr's Portfolio"></a>
 </header>
+<section class="jh-section highlight-section">
+
+</section>
+<section class="jh-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-6">
+				<h1>Created a New Social Network. From Scratch.</h1>
+				<h2>Using HTML, CSS, JavaScript, PHP, Sphinx, and Linux</h2>
+				<p>
+					In January 2013, I began work on <strong>RepFive</strong>, a project that allowed users to follow content based on either the user posting it, 
+					or the category it was posted in.  I moved on from this project in November of 2014 when I accepted a position with Qualtrics.
+				</p>
+				<p>
+					<a href="">Most recent version (preview)</a>
+				</P>
+				<p>
+					<a href="http://www.repfive.com/login.php" targe="_blank">Current Version (RepFive.com)</a>Username:<strong>demo</strong> Password:<strong>repfive</strong>
+				</P>
+			</div>
+			<div class="col-lg-6">
+				<img src="images/screen_repfive.png" alt="RepFive">
+			</div>
+		</div>
+	</div>
+</section>
 
 </body>
 </html>
