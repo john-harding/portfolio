dfedbe34643ef61539089612a3975c61b16b25c8
john-harding <jsh.miami@gmail.com>
2015-08-15 22:35:03 -0600

    analytics

--- index.php
+++ index.php
@@ -14,7 +14,16 @@
 <!-- Font Awesome -->
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 <link rel="icon" href="images/icon_small.ico">
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-66356232-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
  <!-- HTML5 Shim and Respond.js -->
   <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
